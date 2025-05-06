60e316291c56664e9aa22c514fdd7b9681f62a39
Daniil <developmentweb15@gmail.com>
2025-05-06 20:40:54 +0300

    add main app features (getting weather data using visualcrossing API,basic UI)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import "./styles.css";
-const body = document.querySelector("body");
-body.style.backgroundColor="red";
+
+async function getData(cityName){
+    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=us&key=AH3T5E9YZGFT3STYGKWFZH3DD&contentType=json`)
+            .then(function(response){
+                return response.json();
+            })
+            .catch(function(error){
+                console.error(error);
+            })
+}
+class Weather{
+    constructor(dateTime,conditions,temperature){
+        this.dateTime=dateTime;
+        this.conditions=conditions;
+        this.temperature=temperature;
+    }
+}
+function createWeatherObj(cityName){
+    return getData(cityName).then(function(data){
+        const dateTime=data.currentConditions.datetime;
+        const conditions=data.currentConditions.conditions;
+        const temperature=data.currentConditions.temp;
+        return new Weather(dateTime,conditions,temperature);
+    })
+}
+function displayWeather(cityName){
+    createWeatherObj(cityName).then(function(weather){
+        console.log(weather);
+    });
+}
+const form=document.getElementById("form");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const input=document.getElementById("location");
+    displayWeather(input.value);
+    input.value="";
+})
